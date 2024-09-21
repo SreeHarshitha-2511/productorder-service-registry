e99181dc23a321ae8e02da75ebe984d7eb11094f
sreeharshitha <harshithareddy2511@gmail.com>
2024-09-21 22:10:31 +0530

    Initial commit

---
+++ src/test/java/com/sreeharshitha/productorder_service_registry/ProductorderServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreeharshitha.productorder_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductorderServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
